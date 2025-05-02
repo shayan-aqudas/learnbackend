78727a8b2485f60d31055ee24a8666a53e052ddb
Shayan Aqudas <shayanaqudas111@gmail.com>
2025-05-02 08:50:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('your twitter account has been created')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1> PLEASE LOGIN AT OUR WEBSITE <h1>');
+})
+
+app.get('/fun', (req, res) => {
+    res.send('<h2>SHAYAN <br> AQUDAS<h2>');
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
